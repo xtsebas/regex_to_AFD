a20017937028888671d4f745d59a3cf0f643ed02
josuemj <98498772+josuemj@users.noreply.github.com>
2024-09-03 11:20:43 -0600

    Implementación de la pila (Stack.py) para el algoritmo de Shunting Yard

---
+++ Estructuras/Stack.py
@@ -0,0 +1,30 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    
+    def push(self, item):
+        """
+        Inserta un item en el stack
+        """
+        self.items.append(item)
+    
+    def pop(self):
+        """
+        Elimina y devuelve el ultimo elemento en la pila
+        Si la pila esta vacio devuelve None
+        """
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items.pop(0)
+    
+    def peek(self):
+        """
+        Devuelve el ultimo elemento en la pila sin eliminarlo
+        Si la pila esta vacio devuelve None
+        """
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items[0]
